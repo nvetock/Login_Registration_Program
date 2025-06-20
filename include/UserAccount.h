b140e6f68e376f64ed7d8cf822813da51438a6dc
straws <NVetock1@gmail.com>
2025-06-19 22:23:26 -0500

    Revert "Added password encryption logic and implemented user account password get and set methods."
    This reverts commit 3691788b77fd06e34c435963da63181ede47c1a6.

--- include/UserAccount.h
+++ include/UserAccount.h
@@ -2,22 +2,9 @@
 #define USER_ACCOUNT_H
 
 #include <string>
-#include <fstream>
-#include <sstream>
-#include <filesystem>
-#include <iomanip>
-
-#include "Logger.h"
-
-#include "modes.h"
-#include "aes.h"
-#include "filters.h"
-
 
 class UserAccount
 {
-
-
 public:
 
 	UserAccount() {}
@@ -30,33 +17,15 @@ public:
 
 	const std::string& getUsername() const;
 
-	bool verifyPassword(std::string& input_password);
+	const bool verifyPassword(std::string& input_password) const;
 
 	void setPassword(std::string& input_password);
+
 
 private:
 	std::string username{};
 	std::string password{}; // this needs to be encrypted
 
-	// Crypto++ members
-	std::string password_ciphertext{};
-	const std::string account_info_separator{":|:"};
-
-	CryptoPP::byte key[CryptoPP::AES::DEFAULT_KEYLENGTH], iv[CryptoPP::AES::BLOCKSIZE];
-
-
-
-
-
-
-
-	const std::string _password_database_path{ "FakeDB/accountinfo.txt" };
-
-
-	bool setPasswordToFile(std::string& input_password);
-	const std::string getPasswordFromFile() const;
-
-	std::string encryptPassword(std::string& input_password);
 
 	// lost woods
 	//   
--- src/UserAccount.cpp
+++ src/UserAccount.cpp
@@ -3,96 +3,10 @@
 
 const std::string& UserAccount::getUsername() const { return username; }
 
-bool UserAccount::verifyPassword(std::string& input_password) {
-	
-	std::string encryptedPassword{ encryptPassword(input_password) };
+const bool UserAccount::verifyPassword(std::string& input_password) const {
 
-	if (encryptedPassword == getPasswordFromFile()) {
-		return true;
-	}
-
-	return false;
 }
 
 void UserAccount::setPassword(std::string& input_password) {
-	setPasswordToFile(input_password);
-}
 
-
-
-/// Private
-
-std::string UserAccount::encryptPassword(std::string& plaintext) {
-	
-	std::string ciphertext{};
-
-	CryptoPP::AES::DEFAULT_KEYLENGTH;
-
-	CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
-	CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption{ aesEncryption, iv };
-
-	// Create CipherText
-	CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink{ ciphertext });
-	stfEncryptor.Put(reinterpret_cast<const unsigned char*>(plaintext.c_str()), plaintext.length() + 1);
-	stfEncryptor.MessageEnd();
-
-	if (ciphertext == "") {
-		std::cerr << "Error: Ciphertext is empty.";
-	}
-
-	return ciphertext;
-}
-
-
-bool UserAccount::setPasswordToFile(std::string& input_password) {
-
-	std::string encryptedPassword{ encryptPassword(input_password) };
-	
-	// Store ciphertext in FakeDB/accountinfo.txt with username.
-	// 
-	// First check if the directory folder exists, make one if not
-	if (!std::filesystem::is_directory("FakeDB")) {
-		std::filesystem::create_directory("FakeDB");
-	}
-	
-	std::ofstream accountFile(_password_database_path, std::ios::app);
-	if (accountFile.is_open()) {
-
-		accountFile << username << account_info_separator << encryptedPassword << '\n' << std::endl;
-
-		accountFile.close();
-
-		return true;
-	}
-	else {
-		std::cerr << "Error: Unable to open accountinfo file: "
-			<< _password_database_path << std::endl;
-	}
-
-	return false;
-}
-
-
-
-const std::string UserAccount::getPasswordFromFile() const {
-
-	std::ifstream accountFile(_password_database_path);
-	if (accountFile.is_open()) {
-
-		std::string line{""};
-		while (std::getline(accountFile, line)) {
-			int end_account_index{ line.find(account_info_separator) };
-			
-			std::string temp_account{ line.substr(0, end_account_index) };
-
-			if (username == temp_account) {
-				accountFile.close();
-				return line.substr(end_account_index + account_info_separator.length() - 1, line.length());
-			}
-		}
-
-		accountFile.close();
-	}
-	std::cout << "No matching password found. Returning empty.\n";
-	return "";
 }
